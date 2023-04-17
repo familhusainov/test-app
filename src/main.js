289a099dad66c3cf608b5f40aa10e4367f7f3e19
familhusainov <husainovfamil@gmail.com>
2023-04-17 14:36:23 +0400

    main js changes

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import { createApp, h } from 'vue';
 import App from './App.vue'
-import httpAxios from './httpAxios';
-httpAxios.defaults.headers.common['Authorization'] = "token";
 import router from './router';
 import store from "./store"
 
 const app = createApp({
     render() { return h(App); }
 });
-
-
-app.mount('#app');
-app.use(httpAxios);
 app.use(router);
 app.use(store)
+app.mount('#app');
+
 
